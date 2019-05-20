296246465ccd50bd40dcbdd73379801e2171c9f0
Katlin Anderson and Reese Lee <katlin@>
2019-05-20 16:45:36 -0700

    change all bools to ints

--- Assassin/Models/Contract.cs
+++ Assassin/Models/Contract.cs
@@ -14,7 +14,7 @@ namespace Assassin.Models
     public int target_id { get; set; }
     public DateTime contract_start { get; set; }
     public DateTime contract_end { get; set; }
-    public bool fulfillment { get; set; }
+    public int fulfillment { get; set; }
     public string weapon { get; set; }
 
 
--- Assassin/Models/Game.cs
+++ Assassin/Models/Game.cs
@@ -10,8 +10,8 @@ namespace Assassin.Models
     public int id { get; set; }
     public string team_name { get; set; }
     public string password { get; set; }
-    public bool start { get; set; }
-    public bool end { get; set; }
+    public int start { get; set; }
+    public int end { get; set; }
 
     public void BeginGame(int gameId)
     {
@@ -19,7 +19,7 @@ namespace Assassin.Models
       Player.AssignAssassinId();
       foreach (Player player in Player.GetAll())
       {
-        Contract firstContract = new Contract {game_id = gameId, assassin_id = player.assassin_id, target_id = (player.assassin_id + 1), contract_start = DateTime.now, fullfillment = false};
+        Contract firstContract = new Contract {game_id = gameId, assassin_id = player.assassin_id, target_id = (player.assassin_id + 1), contract_start = DateTime.now, fullfillment = 0};
         db.contracts.Add(firstContract);
       }
       db.SaveChanges();
