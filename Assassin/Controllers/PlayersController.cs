300af02617575e5b90cc277ac53388bbc7420055
Brooke Kullberg, Katlin Anderson, Reese Lee and Stuart McKay <reese@>
2019-05-20 18:10:45 -0700

    add if statement to New() for creating new players

--- Assassin/Controllers/PlayersController.cs
+++ Assassin/Controllers/PlayersController.cs
@@ -11,21 +11,37 @@ namespace Assassin.Controllers
     public class PlayersController : Controller
     {
 
-      [HttpGet("player/sign-up")]
+      [HttpGet("/player/sign-up")]
       public IActionResult New()
       {
-          return View();
+        return View();
       }
 
-      [HttpPost("/game/{gameId}/player/sign-up")]
-      public IActionResult CreatePlayer(string gameName, string gamePassword, string name, string email, string password, string phoneNumber, string agentName)
+      [HttpGet("/player/sign-up/authenticate")]
+      public IActionResult AuthenticateGroup(string gameName, string gamePassword)
       {
         var db = new AssassinContext();
-           Game thisGame = db.games.Where(g => g.team_name == gameName && g.password == gamePassword).FirstOrDefault();
-           var player = new Player {name = name, password = password, email = email, code_name = agentName, game_id = thisGame.id, phone_number = phoneNumber};
-           db.players.Add(player);
-           db.SaveChanges();
-          return RedirectToAction("Consent", new {gameId = thisGame.id, id = player.id});
+        Game thisGame = db.games.Where(g => g.team_name == gameName && g.password == gamePassword).FirstOrDefault();
+        if (thisGame == null)
+        {
+          List<string> model = new List<string> {"The team name and/or password is incorrect", "Try again."};
+          return View("New", model);
+        }
+        else
+        {
+          List<Game> GameList = new List<Game> {thisGame};
+          return View("New", GameList);
+        }
+      }
+
+      [HttpPost("/player/sign-up-success")]
+      public IActionResult CreatePlayer(string name, string email, string password, string phoneNumber, string agentName, int gameId)
+      {
+        var db = new AssassinContext();
+        var player = new Player {name = name, password = password, email = email, code_name = agentName, game_id = gameId, phone_number = phoneNumber};
+        db.players.Add(player);
+        db.SaveChanges();
+        return RedirectToAction("Consent", new {gameId = player.game_id, id = player.id});
       }
 
       [HttpGet("/game/{gameId}/player/{id}/consent")]
